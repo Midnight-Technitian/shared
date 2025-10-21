5584eac04976ac1d897e1ae130883d694262728d
Glabay <Java_Guru@live.com>
2025-10-21 18:09:50 -0300

    converted to a record
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/glabay/dtos/UserCredentialsDto.java
+++ src/main/java/dev/glabay/dtos/UserCredentialsDto.java
@@ -1,21 +1,16 @@
 package dev.glabay.dtos;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * @author Glabay | Glabay-Studios
  * @project GlabTech
  * @social Discord: Glabay
  * @since 2024-11-30
  */
-@Getter
-@Setter
-public class UserCredentialsDto {
-    private String firstName;
-    private String lastName;
-    private String contactNumber;
-    private String email;
-    private String password;
-    private String rePassword;
-}
+public record UserCredentialsDto(
+    String firstName,
+    String lastName,
+    String contactNumber,
+    String email,
+    String password,
+    String rePassword
+) {}
