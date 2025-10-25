cc0b7745c8c86222fbbee7724f382ad39004115e
Glabay <Java_Guru@live.com>
2025-10-24 22:53:04 -0300

    Change from record to a class
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/glabay/dtos/ServiceTicketDto.java
+++ src/main/java/dev/glabay/dtos/ServiceTicketDto.java
@@ -1,9 +1,12 @@
 package dev.glabay.dtos;
 
 import dev.glabay.models.ServiceNote;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Glabay | Glabay-Studios
@@ -11,16 +14,121 @@ import java.util.List;
  * @social Discord: Glabay
  * @since 2025-10-24
  */
-public record ServiceTicketDto(
-    String ticketId,
-    String status,
-    String title,
-    String description,
-    String customerId,
-    String customerDeviceId,
-    String employeeId,
-    String serviceId,
-    List<ServiceNote> notes,
-    LocalDateTime createdAt,
-    LocalDateTime updatedAt
-) {}
+public final class ServiceTicketDto {
+    private String ticketId;
+    private String status;
+    private String title;
+    private String description;
+    private String customerId;
+    private String customerDeviceId;
+    private String employeeId;
+    private String serviceId;
+    private List<ServiceNote> notes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerDeviceId() {
+        return customerDeviceId;
+    }
+
+    public void setCustomerDeviceId(String customerDeviceId) {
+        this.customerDeviceId = customerDeviceId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public List<ServiceNote> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<ServiceNote> notes) {
+        this.notes = notes;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceTicketDto{" +
+            "ticketId='" + ticketId + '\'' +
+            ", status='" + status + '\'' +
+            ", title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            ", customerId='" + customerId + '\'' +
+            ", customerDeviceId='" + customerDeviceId + '\'' +
+            ", employeeId='" + employeeId + '\'' +
+            ", serviceId='" + serviceId + '\'' +
+            ", notes=" + notes +
+            ", createdAt=" + createdAt +
+            ", updatedAt=" + updatedAt +
+            '}';
+    }
+}
