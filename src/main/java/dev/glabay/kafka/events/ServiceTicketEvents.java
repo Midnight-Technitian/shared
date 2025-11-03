1368d11c598e564e57f4d9f38b02e5ecfc5808f3
Glabay <Java_Guru@live.com>
2025-11-03 17:10:46 -0400

    Added ServiceTickets Events
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/dev/glabay/kafka/events/ServiceTicketEvents.java
@@ -0,0 +1,16 @@
+package dev.glabay.kafka.events;
+
+import dev.glabay.dtos.ServiceTicketDto;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project frontend
+ * @social Discord: Glabay
+ * @since 2025-11-02
+ */
+public class ServiceTicketEvents {
+    public record ServiceTicketCreatedEvent(ServiceTicketDto requestDto) {}
+    public record ServiceTicketClaimedEvent(ServiceTicketDto requestDto) {}
+    public record ServiceTicketClosedEvent(ServiceTicketDto requestDto) {}
+    public record ServiceTicketUpdatedEvent(ServiceTicketDto requestDto) {}
+}
